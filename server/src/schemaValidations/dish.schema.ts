4cdb8de3016926934b8fb633d1b40e6ac9a4b327
Quoc Thang <comehere.thang@gmail.com>
2024-09-30 12:37:48 +0700

    Bổ sung dish schema
    Bổ sung dish schema

--- server/src/schemaValidations/dish.schema.ts
+++ server/src/schemaValidations/dish.schema.ts
@@ -8,7 +8,7 @@ export const CreateDishBody = z.object({
   description: z.string().max(10000),
   image: z.string().url(),
   status: z.enum(DishStatusValues).optional(),
-  categoryId: z.number()
+  categoryId: z.number().positive()
 })
 
 export type CreateDishBodyType = z.TypeOf<typeof CreateDishBody>
